a1831e23f9f703adf66a97f152aa7d62c9109d60
Hui-Sin <20018354@myrp.edu.sg>
2022-07-08 14:02:59 +0800

    bug 001

--- web/test.php
+++ web/test.php
@@ -1,7 +1,18 @@
-<html>
+<!-- <html>
 <head>
 </head>
 <body>
   Hello World!
+</body>
+</html> -->
+<!DOCTYPE html>
+<html lang="en">
+    <html class="hydrated"><head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
+    <title>Document</title>
+</head>
+<body>
+    
 </body>
 </html>
--- web/testupdated.php
+++ web/testupdated.php
@@ -1,8 +1,14 @@
-<!-- fix : document does not have a HTML doctype -->
-<!DOCTYPE html>
+<!-- fix : document does not have a HTML doctype-->
+<!--<!DOCTYPE html>-->
 <!-- fix : <html> element does not have a [lang] attribute -->
+<!--<html lang="en">
+<head> -->
+<!DOCTYPE html>
 <html lang="en">
-<head>
+    <html class="hydrated"><head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <!-- fix : [user-scalable="no"] is used in the <meta name="viewport"> element or the [maximum-scale] attribute is less than 5. -->
+        <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=6">
 <!-- fix : document does not have <title> -->
 <title>Test php to 100%</title>
 <!-- fix : document does not haveand <meta>,[name] and [content] -->
