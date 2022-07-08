463de507ff9d821fa49f2e7bfe4ae9073e2d0d2c
Hui-Sin <20018354@myrp.edu.sg>
2022-07-08 14:18:01 +0800

    commit bug 1 changes

--- web/test.php
+++ web/test.php
@@ -1,22 +1,5 @@
-<!-- <html>
+<html>
 <head>
-</head>
-<body>
-  Hello World!
-</body>
-</html> -->
-<!DOCTYPE html>
-<html lang="en">
-    
-<html class="hydrated"><head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
-<!-- fix : document does not have <title> -->
-<title>Test php to 100%</title>
-<!-- fix : document does not haveand <meta>,[name] and [content] -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- fix : document does not haveand <meta>,[description] and [content] -->
-<meta name="description" content="Fix the code to obtain a score of 100 for Performance, Accessibility, Best Practices and SEO.">
 </head>
 <body>
   Hello World!
--- web/testupdated.php
+++ web/testupdated.php
@@ -1,15 +1,8 @@
 <!-- fix : document does not have a HTML doctype-->
-<!--<!DOCTYPE html>-->
-<!-- fix : <html> element does not have a [lang] attribute -->
-<!--<html lang="en">
-<head> -->
 <!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
-    <html class="hydrated"><head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <!-- fix : [user-scalable="no"] is used in the <meta name="viewport"> element or the [maximum-scale] attribute is less than 5. -->
-        <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=6">
-<!-- fix : document does not have <title> -->
+<head>
 <title>Test php to 100%</title>
 <!-- fix : document does not haveand <meta>,[name] and [content] -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
