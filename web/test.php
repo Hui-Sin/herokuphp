2f081b5da86dd81d274e1faee199c0cf3c40164c
Hui-Sin <20018354@myrp.edu.sg>
2022-07-08 12:06:13 +0800

    commit change to test.php

--- web/test.php
+++ web/test.php
@@ -1,5 +1,12 @@
-<html>
+<!-- fix : document does not have a HTML doctype -->
+<!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
+<html lang="en">
 <head>
+<!-- fix : document does not have <title> -->
+<title>Test php</title>
+<!-- fix : document does not haveand <meta>,[name] and [content] -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   Hello World!
