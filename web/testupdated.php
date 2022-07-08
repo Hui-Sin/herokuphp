4d7aa6728615bddc0773b41c0f9a90bc64317080
Hui-Sin <20018354@myrp.edu.sg>
2022-07-08 12:53:50 +0800

    commit added orginal code file

---
+++ web/testupdated.php
@@ -0,0 +1,16 @@
+<!-- fix : document does not have a HTML doctype -->
+<!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
+<html lang="en">
+<head>
+<!-- fix : document does not have <title> -->
+<title>Test php to 100%</title>
+<!-- fix : document does not haveand <meta>,[name] and [content] -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- fix : document does not haveand <meta>,[description] and [content] -->
+<meta name="description" content="Fix the code to obtain a score of 100 for Performance, Accessibility, Best Practices and SEO.">
+</head>
+<body>
+  Hello World!
+</body>
+</html>
